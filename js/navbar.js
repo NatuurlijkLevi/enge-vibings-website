41020cbc74e9744227789dee3f2cb46fb050ee6a
NatuurlijkLevi <527898@student.fontys.nl>
2024-07-23 00:22:49 +0200

    finished main styling for the page

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,16 @@
 const body = document.querySelector('body');
+const linkItems = document.querySelectorAll('nav > ul > li');
 let bodyRect = body.getBoundingClientRect(body);
 
 console.log(bodyRect.width, bodyRect.height);
+
+linkItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (item.id !== 'currentpage')
+    {
+        let current = document.getElementById('currentpage');
+        current.id = '';
+        item.id = 'currentpage';
+    }
+  });
+});
