fd25258c948e1c7dd420d2a09941e0d28bf01929
NatuurlijkLevi <527898@student.fontys.nl>
2024-08-04 01:17:08 +0200

    Update Heksensoep and Bubble CSS styles and animations

--- games/js/heksensoep.js
+++ games/js/heksensoep.js
@@ -1,37 +1,89 @@
 const ingredienten = document.querySelectorAll('.ingredient');
 const root = document.querySelector(':root');
 const iframe = document.getElementById('ketel-bubbels')
+const ketelSvgGroup = document.querySelector('div#ketel > svg > g');
 let innerDoc;
 let bubbles;
 let iframeRoot;
+let bubble;
 iframe.onload = () => {
   innerDoc = iframe.contentDocument || iframe.contentWindow.document;
   bubbles = innerDoc.getElementById('bubbles');
   iframeRoot = innerDoc.querySelector(':root');
+  bubble = innerDoc.querySelectorAll('.bubble');
+}
+let ingredientForSoep;
+
+function changeSoupWaterColorByAdding(ingredient){
+  const newLayer = '<path d="M621.5 81C621.5 81.6405 621.197 83.1776 618.606 85.6363C616.013 88.0964 611.777 90.8267 605.635 93.6462C593.396 99.2649 575.212 104.515 552.15 108.993C506.151 117.925 442.277 123.5 371.5 123.5C300.723 123.5 236.849 117.925 190.85 108.993C167.788 104.515 149.604 99.2649 137.365 93.6462C131.223 90.8267 126.987 88.0964 124.394 85.6363C121.803 83.1776 121.5 81.6405 121.5 81C121.5 80.3595 121.803 78.8224 124.394 76.3637C126.987 73.9036 131.223 71.1733 137.365 68.3538C149.604 62.7351 167.788 57.4851 190.85 53.0071C236.849 44.0752 300.723 38.5 371.5 38.5C442.277 38.5 506.151 44.0752 552.15 53.0071C575.212 57.4851 593.396 62.7351 605.635 68.3538C611.777 71.1733 616.013 73.9036 618.606 76.3637C621.197 78.8224 621.5 80.3595 621.5 81Z" fill="var(--' + ingredient + '-insoep)" class="added-color"/>';
+  ketelSvgGroup.innerHTML += newLayer;
+  bubble.forEach(bubbleElement => {
+    const newChild = document.createElement('div');
+    newChild.id = ingredient;
+    let currentElement = bubbleElement;
+    while (currentElement.children.length > 0) {
+      currentElement = currentElement.children[0];
+    }
+    currentElement.appendChild(newChild);
+  })
+  ingredienten.forEach(ingredientenGridItems => {
+    const ingredientenGridItemContent = ingredientenGridItems.innerHTML;
+    ingredientenGridItems.innerHTML = '<div id="' + ingredient + '" class="added-color">' + ingredientenGridItemContent + '</div>'
+    setTimeout(() => {
+      const addedColor = document.querySelector(".added-color");
+      addedColor.classList.remove('added-color');
+    }, 500);
+  })
 }
 
-const soepKleuren = [
-                        'var(--groene-soep)',
-                        'var(--rode-soep)',
-                        'var(--blauwe-soep)',
-                        'var(--gele-soep)',
-                        'var(--paarse-soep)',
-                        'var(--oranje-soep)',
-                        'var(--witte-soep)',
-                        'var(--bruine-soep)',
-                        'var(--roze-soep)',
-                        'var(--turquoise-soep)'
-                    ];
+function sipSoep() {
+  const randomNumber = Math.random();
+  const forward = [5, 6, 7, 8]
+  const goodResponse = 
+                      [
+                        "Het smaakt heerlijk!",
+                        "De soep is verrukelijk!",
+                        "De soep is om je vingers bij af te likken!",
+                        "Het is om van te smullen!",
+                        "De soep is zo lekker dat je "
+                      ]
+  const backward = [1, 2, 3, 4]
+  const badResponse = 
+                      [
+                        "Het smaakt afschuwelijk!",
+                        "De soep is niet te drinken!",
+                        "Het is niet te eten!",
+                        "De soep is niet te vreten!",
+                        "De soep is zo vies dat je "
+                      ]
+  const moveResponses = 
+                      [
+                        "vakjes",
+                        "stappen",
+                        "hokjes",
+                      ]
+
+  alert("Je neemt een slok...");
+  if (randomNumber < 0.5) {
+    
+  }
+  else {
+    
+  }
+  
+}
 
 ingredienten.forEach(ingredient => {
   ingredient.addEventListener('click', () => {
-    if (!ingredient.classList.contains('used')) {
-      const ingredientImg = ingredient.children[0].children[0];
+    if (!ingredient.classList.contains('used')) {      
+      let deepestChildParent = ingredient;
+      while (deepestChildParent.children.length < 2) {
+        deepestChildParent = deepestChildParent.children[0];
+      }
+      const ingredientImg = deepestChildParent.children[0].children[0];
+      const ingredientName = deepestChildParent.children[1].innerText;
       const ingredientImgRect = ingredientImg.getBoundingClientRect();
       ingredient.classList.toggle('used');
-      const nieuweSoepKleur = soepKleuren[Math.floor(Math.random() * soepKleuren.length)];
-      root.style.setProperty('--huidige-soepkleur', nieuweSoepKleur);
-      iframeRoot.style.setProperty('--huidige-soepkleur', nieuweSoepKleur);
 
       // Create a new image element
       const imgElement = document.createElement('img');
@@ -42,6 +94,16 @@ ingredienten.forEach(ingredient => {
 
       // Append the new image element to the bubbles container
       bubbles.appendChild(imgElement);
+      setTimeout(() => {
+        ingredientForSoep = ingredientName.toLowerCase().replace('’', '');
+        while (ingredientForSoep.includes(' ')) {
+          ingredientForSoep = ingredientForSoep.replace(' ', '');
+        }
+        changeSoupWaterColorByAdding(ingredientForSoep);
+        setTimeout(() => {
+          imgElement.style.opacity = 1;
+        }, 200);
+      }, 1250);
     } else {
       alert(`Je hebt dit ingrediënt al gebruikt!`);
     }
