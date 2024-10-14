855fafd90aec9e6e4d38871f3d29bcb78232c4d1
NatuurlijkLevi <levimeijer@live.nl>
2024-10-14 13:29:38 +0200

    chore: Added functionality so you can open a horror game via the website

--- js/challenges.js
+++ js/challenges.js
@@ -18,6 +18,7 @@ const banaanContainerAnimation = "bananaAppear 2500ms ease-in-out";
 const banaanAnimation = "bananaShootToCloseChallenges 2500ms ease-in-out";
 
 let animationBusy = false;
+const horrorSpelButton = document.getElementById("horrorspel");
 
 // Add an event listener to the challenge button
 // When the button is clicked, the challenge container will slide out
@@ -57,3 +58,63 @@ challengeButton.addEventListener("click", () => {
         }, 5000);
     }
 });
+
+let horrorSpelOgenOne;
+let horrorSpelOgenTwo;
+let horrorSpelPopUpButton;
+
+function showPopUp() {
+    const horrorSpelPopUp = document.createElement("div");
+    horrorSpelPopUp.innerHTML += `  <div id="horrorspel-popup">
+                                        <h1>Hoeveel ogen?</h1>
+                                        <div id="ogen-container">
+                                            <input type="number" id="ogen1" min="1" max="6">
+                                            <input type="number" id="ogen2" min="1" max="6">
+                                        </div>
+                                        <button>Open horrorspel</button>
+                                        <a href="horrorspel-alternatief:">Open alternatief</a>
+                                    </div>`;
+    document.body.appendChild(horrorSpelPopUp);
+    horrorSpelOgenOne = document.getElementById("ogen1");
+    horrorSpelOgenTwo = document.getElementById("ogen2");
+    horrorSpelPopUpButton = document.querySelector("#horrorspel-popup > button");
+    horrorSpelPopUpAlt = document.querySelector("#horrorspel-popup > a");
+    horrorSpelPopUpButton.addEventListener("click", openHorrorSpel);
+    horrorSpelPopUpAlt.addEventListener("click", openHorrorSpelAlt);
+}
+
+horrorSpelButton.addEventListener("click", showPopUp);
+
+function removeContainer(container){
+    horrorSpelOgenOne = null;
+    horrorSpelOgenTwo = null;
+    horrorSpelPopUpButton = null;
+    horrorSpelPopUpAlt = null;
+    
+    container.remove();
+}
+
+// Add an event listener to the horror spel button
+function openHorrorSpel() {
+    const horrorSpelContainer = document.querySelector("div:has(> #horrorspel-popup)");
+    if (horrorSpelContainer && horrorSpelOgenOne.value !== "" && horrorSpelOgenTwo.value !== "" && horrorSpelOgenOne.value > 0 && horrorSpelOgenOne.value < 7 && horrorSpelOgenTwo.value > 0 && horrorSpelOgenTwo.value < 7)
+    {   
+        const ogenOne = horrorSpelOgenOne.value;
+        const ogenTwo = horrorSpelOgenTwo.value;
+        window.open(`horrorspel-${ogenOne}-${ogenTwo}:`);
+        removeContainer(horrorSpelContainer);
+        horrorSpelPopUpButton.removeEventListener("click", openHorrorSpel);
+        horrorSpelPopUpAlt.removeEventListener("click", openHorrorSpelAlt);
+    }
+}
+
+// Add an event listener to the horror spel alt button
+function openHorrorSpelAlt() {
+    const horrorSpelContainer = document.querySelector("div:has(> #horrorspel-popup)");
+    if (horrorSpelContainer) {
+        window.open("horrorspel-alternatief:");
+        removeContainer(horrorSpelContainer);
+        horrorSpelPopUpButton.removeEventListener("click", openHorrorSpel);
+        horrorSpelPopUpAlt.removeEventListener("click", openHorrorSpelAlt);
+    }
+}
