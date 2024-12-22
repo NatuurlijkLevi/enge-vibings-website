0f5a0af5cb086fa01887234e383990937ec346c8
NatuurlijkLevi <527898@student.fontys.nl>
2024-12-22 23:57:38 +0100

    feat: add kerstkaarten game functionality; implement random card drawing with JSON and update HTML references

--- games/js/kerstkaarten.js
+++ games/js/kerstkaarten.js
@@ -1,16 +1,16 @@
 // Initialize variables
-const koningButton = document.querySelector("button");
-const koningH1 = document.querySelector("h1");
-const koningP = document.querySelector("p");
+const kerstmanButton = document.querySelector("button");
+const kerstmanH1 = document.querySelector("h1");
+const kerstmanP = document.querySelector("p");
 
-let koningDied = false;
+let kerstmanDied = false;
 let lastTimeNegative = false;
     
 // function to draw a random card
 async function drawRandomCard()
 {
-    // Fetch the koningkaarten.json file
-    const response = await fetch("json/koningkaarten.json");
+    // Fetch the kerstkaarten.json file
+    const response = await fetch("json/kerstkaarten.json");
     // Get the data from the response
     const data = await response.json();
     // Get a random number between 0 and 1
@@ -32,27 +32,27 @@ async function drawRandomCard()
     // Get a random card from the cardArray
     let cardValue = Math.floor(Math.random() * cardArray.length);
     // If the number is 0 and the king died by that same card that has already been drawn, draw a new card
-    if (koningDied) {
+    if (kerstmanDied) {
         while (cardValue === 0)
         {
             cardValue = Math.floor(Math.random() * cardArray.length);
         }
-        koningDied = false;
+        kerstmanDied = false;
     }
-    // If the number is 0 and this turn the card is negative, set the koningDied variable to true
+    // If the number is 0 and this turn the card is negative, set the kerstmanDied variable to true
     if (cardValue === 0 && lastTimeNegative)
     {
-        koningDied = true;
+        kerstmanDied = true;
     }
 
     // Get the card from the cardArray and set the innerHTML of the h1 and p elements
-    koningH1.innerHTML = cardArray[cardValue][0];
-    koningP.innerHTML = cardArray[cardValue][1];
+    kerstmanH1.innerHTML = cardArray[cardValue][0];
+    kerstmanP.innerHTML = cardArray[cardValue][1];
     // Set the specialActionDone variable to koningkaarten
     localStorage.setItem("specialActionDone", "koningkaarten");
 }    
 
 // If you press the button, draw a random card
-koningButton.addEventListener('click', function() {
+kerstmanButton.addEventListener('click', function() {
     drawRandomCard();
 });
